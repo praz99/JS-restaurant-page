a70354501b6b29438b9af0c0e0e883e0c18da591
praz <t.prazwal@gmail.com>
2020-10-23 15:42:20 +0200

    add navigation bar

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
 import './style.css';
 
+// const body = document.querySelector('body');
 const content = document.getElementById('content');
-content.classList.add('main-page');
+const navBar = document.createElement('nav');
+const ul = document.createElement('ul');
+const li = document.createElement('li');
+
+const insideNavbar = () => {
+  navBar.classList.add('navbar');
+  const navLeft = document.createElement('div');
+  navLeft.classList.add('nav-left');
+  navLeft.innerText = 'Rest-Your-Ant';
+  const navRight = document.createElement('div');
+  navRight.classList.add('nav-right');
+  navRight.innerHTML = navLinks();
+  navBar.appendChild(navLeft);
+  navBar.appendChild(navRight);
+  document.body.appendChild(navBar);
+};
+
+const navLinks = () => {
+  return ['menu', 'about', 'contact'];
+};
+
+insideNavbar();
+
+content.classList.add('home-page');
