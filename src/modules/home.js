fc3ffb57af7b33bc2a11aae49927b045ea6c7782
praz <t.prazwal@gmail.com>
2020-10-26 00:42:35 +0100

    add background to home page

--- src/modules/home.js
+++ src/modules/home.js
@@ -5,8 +5,11 @@ const content = document.getElementById('content');
 const showHome = () => {
   const name = document.createElement('div');
   name.classList.add('home-welcome');
-
   content.appendChild(name);
+
+  const homeHeading = document.createElement('h1');
+  homeHeading.innerText = 'Welcome!';
+  name.appendChild(homeHeading);
 };
 
 export { showHome };
